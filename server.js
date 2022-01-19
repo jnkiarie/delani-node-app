425a3668a2be1f6d99eedff80b95bdc4ba0ccb28
jnkiarie <92355202+jnkiarie@users.noreply.github.com>
2022-01-19 23:30:08 +0300

    server code worked well

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 
-let http = require('http');
+let http = require('http'); // imports the http module that is shipped together with Node.js 
+//when we install it and makes it accessible through the variable http
 
 
-http.createServer(function(request, response){
- response.writeHead(200, {"Content-type": "text/plain"});
- response.write("Hello World");
- response.end();
+http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-type": "text/plain" });
+    response.write("Jimmy You made your First Server");
+    response.end();
 }).listen(8000);
 
