8805be7a172fbcb6fdc868de47422defd2786431
jnkiarie <92355202+jnkiarie@users.noreply.github.com>
2022-01-31 04:43:19 +0300

    add server module to index js

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 
 let http = require('http'); // imports the http module that is shipped together with Node.js 
 //when we install it and makes it accessible through the variable http
+let url = require('url');
 
 
-http.createServer(function (request, response) {
-    response.writeHead(200, { "Content-type": "text/plain" });
-    response.write("Jimmy You made your First Server");
+// http.createServer(function (request, response) {
+//     response.writeHead(200, { "Content-type": "text/plain" });
+//     response.write("Jimmy You made your First Server");
+//     response.end();
+// }).listen(8000);
+
+// A function to wrap our server functionality so that we can export it
+let start = function(){
+function onRequest(request, response){
+    // Extracting the pathname from the url requested
+    let pathname = url.parse(request.url).pathname
+    console.log(pathname);
+    console.log("Request Received")
+    response.writeHead(200, {"Content-type": "text/plain"});
+    response.write("Server Creation Complete ");
+    response.write('We are live!!');
     response.end();
-}).listen(8000);
+}
+
+http.createServer(onRequest).listen(8000);
+console.log("Server has Started!")
+}
+
+exports.start = start;
+
+
 
